c750f3c5601cc8c032d47493aa0ce93dc0d7aae4
Jeremy Lainé <jeremy.laine@m4x.org>
2017-05-29 11:23:59 +0200

    bump version to 0.5.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='djangorestsearch',
-    version='0.5.5',
+    version='0.5.6',
     url='https://github.com/wemap/django-rest-search',
     license='BSD',
     description='ElasticSearch integration for Django.',
