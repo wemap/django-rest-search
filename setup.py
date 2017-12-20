1262b4fb00a2b68a7eb29373f1aefab639e1d19e
Jeremy Lainé <jeremy.laine@m4x.org>
2017-12-21 00:35:53 +0100

    [setup] mark as python 3 only

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3 :: Only",
     ],
     install_requires=[
         'aws-requests-auth>=0.3.0',
         'certifi>=2017.4.17',
         'elasticsearch>=5.0.0,<6.0.0',
     ],
+    python_requires='>=3',
 )
