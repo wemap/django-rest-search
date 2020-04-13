695c6bfb07400b4c3b0f68ea6fefa773fa782947
Jeremy Lainé <jeremy.laine@m4x.org>
2020-04-13 15:22:47 +0200

    v0.8.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='djangorestsearch',
-    version='0.8.5',
+    version='0.8.6',
     url='https://github.com/wemap/django-rest-search',
     license='BSD',
     description='ElasticSearch integration for Django.',
